9d36ebf32f66a338df0a5ba21a5e7ccb3a75d4d3
Matheus Campos <245040149+matcxz@users.noreply.github.com>
2026-09-20 12:34:59 -0300

    update dots

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -9,18 +9,26 @@ alias ll='eza -lah --icons always'
 alias la='eza -a --icons always'
 alias l='eza -lah --icons always'
 
-alias v='vim'
 alias vim='nvim'
 alias pacs='doas pacman -S'
 alias pacsyu='doas pacman -Syu'
 alias pacrns='doas pacman -Rns'
+alias pacr='doas pacman -R'
 
 alias rm='rm -rf'
 
 alias cd='z'
 alias fast='fastfetch'
+alias c='clear'
 
 # Zoxide
 zoxide init fish | source
 
-pfetch
+# Starship
+starship init fish | source
+
+function starship_transient_prompt_func
+    starship module character
+end
+
+enable_transience
